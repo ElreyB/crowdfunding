0bed30ae94a74defe2f7e63544a65e3e2e91c4ab
Elrey Belmonti and Megan Olson <elrey@>
2017-11-01 10:14:58 -0700

    create project component

--- src/app/project/project.component.ts
+++ src/app/project/project.component.ts
@@ -1,16 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { Project } from '../project.model';
+import { ProjectService } from '../project.service';
+import { Router } from '@angular/router';
+import { FirebaseListObservable } from 'angularfire2/database';
+
 
 
 @Component({
   selector: 'app-project',
   templateUrl: './project.component.html',
-  styleUrls: ['./project.component.css']
+  styleUrls: ['./project.component.css'],
+  providers: [ProjectService]
 })
 export class ProjectComponent implements OnInit {
+  projects: FirebaseListObservable<any[]>;
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private projectService: ProjectService
+  ) { }
 
   ngOnInit() {
+    this.projects = this.projectService.getProjects();
+  }
+
+  goToProjectPage(clickedProject) {
+    this.router.navigate(['projects', clickedProject.$key]);
   }
 
 }
